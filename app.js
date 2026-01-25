4c086f506f396b43c895fbb4e4cfde432ea64e7f
pauljoelpauljoel <pauljoelpauljoel408@gmail.com>
2026-01-25 12:12:58 +0530

    feat: Implement initial website layout, styling, and navigation with header and logo assets.

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     const langFile = require(`./locales/${lang}.json`);
     res.locals.t = langFile;
     res.locals.currentLang = lang;
+    res.locals.currentPath = req.path;
 
     next();
 });
